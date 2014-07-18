452976a95be74cdeeaf18c9c8f104ee209adf570
Umair <umair.vatao@2359media.com>
2014-07-18 18:11:50 +0800

    Changed the interface structure

--- AnimationLibrary/src/com/siyao/animationlibrary/Animation.java
+++ AnimationLibrary/src/com/siyao/animationlibrary/Animation.java
@@ -3,7 +3,5 @@ import android.view.View;
 
 
 public abstract class Animation {
-	
 	public abstract void animate(View view);
-
 }
--- AnimationLibrary/src/com/siyao/animationlibrary/MyAnimator.java
+++ AnimationLibrary/src/com/siyao/animationlibrary/MyAnimator.java
@@ -7,5 +7,21 @@ public class MyAnimator {
 	public static void animate(View view, Animation animation) {
 		animation.animate(view);
 	}
+	
+	public static void explode(View v) {
+		 new ExplodeAnimation().animate(v);
+	}
+	
+	
+	
+	public static void explode(View v,int xParts, int yParts,long duration) {
+		new ExplodeAnimation(xParts, yParts, duration).animate(v);
+	}
+	
+	public static void highlight(View v){
+		new HighlightAnimation().animate(v);
+	}
 
 }
+
+ 
