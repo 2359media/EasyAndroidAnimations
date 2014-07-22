65f16e72765195a2cb578947687df52470d4abcb
Si Yao <neesiyao@gmail.com>
2014-07-22 17:46:51 +0800

    Finalised TransferAnimation1

--- AnimationLibrary/src/com/androidanimator/animation/TransferAnimation1.java
+++ AnimationLibrary/src/com/androidanimator/animation/TransferAnimation1.java
@@ -1,20 +1,7 @@
 package com.androidanimator.animation;
 
-import android.animation.Animator;
-import android.animation.Animator.AnimatorListener;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
-import android.animation.ValueAnimator.AnimatorUpdateListener;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-
-import com.siyao.animationlibrary.R;
 
 /**
  * The TransferAnimation1 transfers the view to another view provided by the
@@ -27,7 +14,7 @@ public class TransferAnimation1 extends Animation {
 
 	View destinationView;
 	ViewGroup parentView;
-	float positionX, positionY;
+	int transX, transY;
 	
 	public TransferAnimation1(View destinationView) {
 		this.destinationView = destinationView;
@@ -35,23 +22,7 @@ public class TransferAnimation1 extends Animation {
 	
 	@Override
 	public void animate(final View view) {
-		parentView = (ViewGroup) view.getParent();
-		final FrameLayout transferFrame = new FrameLayout(view.getContext());
-		//final ImageView transferBox = new ImageView(view.getContext());
-		LayoutParams layoutParams = view.getLayoutParams();
-		transferFrame.setLayoutParams(layoutParams);
-		//transferBox.setLayoutParams(layoutParams);
-		//transferBox.setBackgroundResource(R.drawable.border);
-		int[] locationOriginalView = new int[2];
-		view.getLocationOnScreen(locationOriginalView);
-		Log.d("locationOriginalView", locationOriginalView[0] + " " + locationOriginalView[1]);
-		
-		int positionView = parentView.indexOfChild(view);
-		//parentView.removeView(view);
-		//transferFrame.addView(view);
-		//transferFrame.addView(transferBox);
-		//parentView.addView(transferFrame, positionView);
-		
+		parentView = (ViewGroup) view.getParent();		
 		final ViewGroup rootView = (ViewGroup) view.getRootView();
 		while (!parentView.equals(rootView)) {
 			parentView.setClipChildren(false);
@@ -61,52 +32,20 @@ public class TransferAnimation1 extends Animation {
 		
 		final float scaleX = (float) destinationView.getWidth() / ((float) view.getWidth()),
 				scaleY = (float) destinationView.getHeight() / ((float) view.getHeight());
-		
-		int[] locationDest = new int[2];
-		int[] locationView = new int[2];
-		//int[] locationTB = new int[2];
-		//transferBox.getLocationOnScreen(locationTB);
-		int transX, transY;
+		int[] locationDest = new int[2], locationView = new int[2];
 		view.getLocationOnScreen(locationView);
 		destinationView.getLocationOnScreen(locationDest);
-		
-		Log.d("locationView", locationView[0] + " " + locationView[1]);
-		Log.d("locationDest", locationDest[0] + " " + locationDest[1]);
-		//Log.d("locationTB", locationTB[0] + " " + locationTB[1]);
-		
-		
-		
-		if (locationView[0] < locationDest[0])
-			transX = locationDest[0] - locationView[0]; 
-		else
-			transX = locationView[0] - locationDest[0];
-		
-		if (locationView[1] > locationDest[1])
-			transY = locationDest[1] - locationView[1]; 
-		else
-			transY = locationView[1] - locationDest[1];
-		
-		transX = transX - view.getWidth()/2 + destinationView.getWidth();
-		transY = transY - view.getHeight()/2 + destinationView.getHeight();
-		
-		final ValueAnimator scaleXAnim = ObjectAnimator.ofFloat(view, View.SCALE_X, scaleX),
-				scaleYAnim = ObjectAnimator.ofFloat(view, View.SCALE_Y, scaleY),
-				translateXAnim = ObjectAnimator.ofFloat(view, View.TRANSLATION_X, transX),
-				translateYAnim = ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, transY),
-				alphaAnim = ObjectAnimator.ofFloat(view, View.ALPHA, 0.5f);
-		final AnimatorSet animatorSet = new AnimatorSet();
-		animatorSet.playTogether(translateXAnim, translateYAnim, alphaAnim);
-		animatorSet.setDuration(duration);
-		animatorSet.start();
-//				animatorSet.addListener(new AnimatorListenerAdapter() {
-//					
-//					@Override
-//					public void onAnimationEnd(Animator animation) {
-//						scaleXAnim.reverse();
-//						scaleYAnim.reverse();
-//						translateXAnim.reverse();
-//						translateYAnim.reverse();
-//					}
-//				}); 
+		transX = locationDest[0] - locationView[0];
+		transY = locationDest[1] - locationView[1];
+		transX = transX - view.getWidth()/2 + destinationView.getWidth()/2;
+		transY = transY - view.getHeight()/2 + destinationView.getHeight()/2;
+
+		view.animate().scaleX(scaleX).scaleY(scaleY).translationX(transX).translationY(transY).setDuration(duration).withEndAction(new Runnable() {
+			
+			@Override
+			public void run() {
+				view.animate().scaleX(1).scaleY(1).translationXBy(-transX).translationYBy(-transY);
+			}
+		});
 	}
 }
