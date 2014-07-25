f624a8e303ba6af25ad37b767a272f41202989e8
Phu Tang <phu.tanghong@2359media.com.vn>
2014-07-25 08:45:15 +0800

    Initial demo project

---
+++ project/src/com/two359media/animationsample/AnimationDetailActivity.java
@@ -0,0 +1,69 @@
+package com.two359media.animationsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+/**
+ * An activity representing a single Animation detail screen. This activity is
+ * only used on handset devices. On tablet-size devices, item details are
+ * presented side-by-side with a list of items in a
+ * {@link AnimationListActivity}.
+ * <p>
+ * This activity is mostly just a 'shell' activity containing nothing more than
+ * a {@link AnimationDetailFragment}.
+ */
+public class AnimationDetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_animation_detail);
+
+		// Show the Up button in the action bar.
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		// savedInstanceState is non-null when there is fragment state
+		// saved from previous configurations of this activity
+		// (e.g. when rotating the screen from portrait to landscape).
+		// In this case, the fragment will automatically be re-added
+		// to its container so we don't need to manually add it.
+		// For more information, see the Fragments API guide at:
+		//
+		// http://developer.android.com/guide/components/fragments.html
+		//
+		if (savedInstanceState == null) {
+			// Create the detail fragment and add it to the activity
+			// using a fragment transaction.
+			Bundle arguments = new Bundle();
+			arguments.putString(
+					AnimationDetailFragment.ARG_ITEM_ID,
+					getIntent().getStringExtra(
+							AnimationDetailFragment.ARG_ITEM_ID));
+			AnimationDetailFragment fragment = new AnimationDetailFragment();
+			fragment.setArguments(arguments);
+			getFragmentManager().beginTransaction()
+					.add(R.id.animation_detail_container, fragment).commit();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == android.R.id.home) {
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpTo(this, new Intent(this,
+					AnimationListActivity.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ project/src/com/two359media/animationsample/AnimationDetailFragment.java
@@ -0,0 +1,63 @@
+package com.two359media.animationsample;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.two359media.animationsample.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Animation detail screen. This fragment is
+ * either contained in a {@link AnimationListActivity} in two-pane mode (on
+ * tablets) or a {@link AnimationDetailActivity} on handsets.
+ */
+public class AnimationDetailFragment extends Fragment {
+	/**
+	 * The fragment argument representing the item ID that this fragment
+	 * represents.
+	 */
+	public static final String ARG_ITEM_ID = "item_id";
+
+	/**
+	 * The dummy content this fragment is presenting.
+	 */
+	private DummyContent.DummyItem mItem;
+
+	/**
+	 * Mandatory empty constructor for the fragment manager to instantiate the
+	 * fragment (e.g. upon screen orientation changes).
+	 */
+	public AnimationDetailFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (getArguments().containsKey(ARG_ITEM_ID)) {
+			// Load the dummy content specified by the fragment
+			// arguments. In a real-world scenario, use a Loader
+			// to load content from a content provider.
+			mItem = DummyContent.ITEM_MAP.get(getArguments().getString(
+					ARG_ITEM_ID));
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_animation_detail,
+				container, false);
+
+		// Show the dummy content as text in a TextView.
+		if (mItem != null) {
+			((TextView) rootView.findViewById(R.id.animation_detail))
+					.setText(mItem.content);
+		}
+
+		return rootView;
+	}
+}
