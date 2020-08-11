74ed27bba6a3bcb1bb61e0575f001f092cd5b50f
yapdianhao <yapdhao@gmail.com>
2020-08-12 02:05:12 +0800

    started revamp project

---
+++ AndroidAnimationDemo/app/src/main/java/com/example/androidanimationdemo/AnimationDetailFragment.java
@@ -0,0 +1,4 @@
+package com.example.androidanimationdemo;
+
+public class AnimationDetailFragment {
+}
---
+++ AndroidAnimationDemo/app/src/main/java/com/example/androidanimationdemo/AnimationListActivity.java
@@ -0,0 +1,8 @@
+package com.example.androidanimationdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AnimationListActivity {
+}
